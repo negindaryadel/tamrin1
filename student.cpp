d517d8d10b912c8d9251871db6b6039373c61cbe
negin <83733021+negindaryadel@users.noreply.github.com>
2021-10-05 12:39:57 +0330

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string name,family;
+   float nomre,a,b,c,average=0;
+   cout<<"enter name and family"<<endl;
+   cin>>name;
+   cin>>family;
+   cout<<"enter 3nomre"<<endl;
+   cin>>a;
+   cin>>b;
+   cin>>c;
+   average=(a+b+c)/3;
+
+   if(average>=17)
+   {
+       cout<<name<<family<<" : ";
+       cout<<"great";
+   }
+
+  else if(average<17 && average>=12 )
+   {
+       cout<<name<<family<<" : ";
+       cout<<"normal";
+   }
+ else if( average<12)
+   {
+       cout<<name<<family<<" : ";
+       cout<<"Faill";
+   }
+
+   return 0;
+}
